83c752b81650e4c5848f5fc64df7760155e67285
youichirou-uno <yoichiro.uno@gmail.com>
2018-08-23 20:43:11 +0900

    count test

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ def draw_positions(frame, decoded_objs):
                  
     font = cv2.FONT_HERSHEY_SIMPLEX
 #    cv2.putText(frame,Detected QR codes: str(count),(0,0), font, 4,(0,255,0),2,cv2.LINE_AA)
-    cv2.putText(frame,str(count),(10,10), font, 4,(0,255,0),2,cv2.LINE_AA)
+    cv2.putText(frame,str(count),(10,100), font, 4,(0,255,0),2,cv2.LINE_AA)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=False, threaded=True)
